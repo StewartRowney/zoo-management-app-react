88d6c0c2b86befd4bb91798c6b5fc10ce1b7f5f9
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2024-01-25 15:03:26 +0000

    Commit of get all zoos list and initial commit of pages/components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/LayoutPage";
+import Home from "./pages/HomePage";
+import Contact from "./pages/ContactPage";
+import NoPage from "./pages/NoPage";
+import Zoos from "./pages/ZoosPage";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="zoos" element={<Zoos />}/>
+      <Route path="*" element={<NoPage />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+
   );
 }
-
-export default App;
