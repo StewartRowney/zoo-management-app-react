831a18e1e06a49b3beea9bfb266789a43955d7fb
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2024-02-02 15:23:54 +0000

    Changed gif

--- src/components/NoPageComponent.js
+++ src/components/NoPageComponent.js
@@ -8,7 +8,7 @@ const NoPageComponent = () => {
                 <h1 className="animal-h1">404 Page Not found</h1>
             </div>
             <div className="nopage-gif">
-                <iframe src="https://giphy.com/embed/1bAdvIjqaXCSc" class="nopage-gif-item"></iframe>
+                <iframe src="https://giphy.com/embed/hEc4k5pN17GZq" class="nopage-gif-item"></iframe>
             </div>
         </div>
     );
