e4442905ab2a1856e56f2adcaae4f64e8ced7c11
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2024-02-06 16:40:44 +0000

    fixed delete api

--- src/apis/deleteApi.js
+++ src/apis/deleteApi.js
@@ -1,11 +1,9 @@
 
-
-
-const deleteItem = (type, collection, item, setCollection) => {
-    fetch('http://localhost:8080/' + type + '/' + item.id, {method: 'DELETE'})
+const deleteItem = (type, item) => {
+    return fetch('http://localhost:8080/' + type + '/' + item.id, {method: 'DELETE'})
         .then(() => {
           console.log(item.name + " deleted");
-          setCollection(collection.filter(i => i !== item));})
+          return true;})
         .catch(error => console.error('Error deleting ' + item.name + ':', error));
   };
 
