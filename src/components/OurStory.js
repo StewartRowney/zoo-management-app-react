e062682ac9f097abf4a9a12a8071ba6c5c0bc6c9
abhi1 <abhi1904kale@gmail.com>
2024-02-06 11:07:17 +0000

    fixed css

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -11,16 +11,16 @@ const OurStory = () => {
               <h1 className="animal-h1">Our Story</h1>
           </div>
         <div>
-            <h2 className='title-h2'>Who we are</h2>
+            <h2 className='title-h2 big'>Who we are</h2>
             <p className='tab'>We are bug buster and working zoo management app. Our story is one of dedication to creating a haven for animals, fostering education, and inspiring a deep connection between people and the natural world. Our journey began as four individuals with a shared love for animals and a desire to make a difference. United by our common goal, we embarked on this project with the aim of developing a user-friendly and efficient platform to streamline zoo operations and promote wildlife conservation.</p>
             <p className='tab'>Behind every line of code, every carefully crafted exhibit, and every educational program, there's a shared passion that unites our team – a commitment to wildlife conservation and environmental stewardship. We believe that by creating a space where people can connect with animals and nature, we contribute to a collective effort to protect the planet and its inhabitants. </p>
-            <h2 className='title-h2'>What we do</h2>
+            <h2 className='title-h2 big'>What we do</h2>
             <p className='tab'>At the heart of our project lies a commitment to leveraging technology for the greater good of wildlife and the environment. We believe that by empowering zoos with efficient management tools and promoting awareness about conservation efforts, we can contribute to the protection and preservation of our planet's biodiversity.</p>
-            <h3 className='title-h3'>The Team Behind the Dream</h3>
-            <li>Stewart</li>
-            <li>Ayush</li>
-            <li>Thomas</li>
-            <li>Abhijeet</li>
+            <h3 className='title-h3 big'>The Team Behind the Dream</h3>
+            <li className='tab'>Stewart</li>
+            <li className='tab'>Ayush</li>
+            <li className='tab'>Thomas</li>
+            <li className='tab'>Abhijeet</li>
         </div>
       </div>
       
